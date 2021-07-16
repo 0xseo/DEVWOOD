1be741455c61e5261f0493786a6ae187b1614c40
0xseo <0xseo@yonsei.ac.kr>
2021-07-16 18:22:47 +0900

    web scraping 1~3

---
+++ 3_requests.py
@@ -0,0 +1,16 @@
+import requests
+
+nav = requests.get("http://naver.com")
+goo = requests.get("http://google.com")
+# nav.status_code : 링크의 정보를 제대로 받았는가 (200:정상 / 403:접근권한x)
+
+# if nav.status_code == requests.codes.ok:
+#     print("정상")
+# else:
+#     print("Error code", nav.status_code)
+
+goo.raise_for_status() # 정보를 받을 수 없다면 에러를 발생시킴
+
+print(goo.text)
+with open("mygoogle.html", "w", encoding="utf8") as f: # mygoogle.html 파일에 쓰기 (utf8로)
+    f.write(goo.text)
